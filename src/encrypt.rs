7f04447aa1917e1e126b1bdaa28bada8ef405a9d
Mark <mverleg.noreply@gmail.com>
2020-01-19 23:05:09 +0100

    Encryption cli arguments

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -1,4 +1,41 @@
+use ::file_endec::EncryptConfig;
+use ::structopt::StructOpt;
+use std::path::PathBuf;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "FileEnc", about = "Securely encrypt one or more files using the given key.")]
+pub struct EncryptArguments {
+
+    //TODO @mark: at least 1
+    #[structopt(name = "FILES", parse(from_os_str))]
+    files: Vec<PathBuf>,
+
+    #[structopt(short = "k", long)]
+    key: Option<String>,
+
+    #[structopt(short = "v", long)]
+    debug: bool,
+
+    #[structopt(short = "f", long)]
+    overwrite: bool,
+
+    #[structopt(short = "d", long)]
+    delete_input: bool,
+
+    #[structopt(parse(from_os_str), short = "o", long)]
+    output_dir: Option<PathBuf>,
+
+    #[structopt(short = "e", long)]
+    output_extension: Option<String>,
+}
+
+impl From<EncryptArguments> for EncryptConfig {
+    fn from(args: EncryptArguments) -> Self {
+        unimplemented!()
+    }
+}
 
 pub fn main() {
-    println!("encrypt");
+    let args = EncryptArguments::from_args();
+    println!("{:?}", args);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,22 @@
 pub mod util;
 pub mod header;
+
+use crate::util::FedResult;
+
+#[derive(Debug)]
+pub struct EncryptConfig {
+
+}
+
+#[derive(Debug)]
+pub struct DecryptConfig {
+
+}
+
+pub fn encrypt(config: &EncryptConfig) -> FedResult<()> {
+    unimplemented!()  //TODO @mark:
+}
+
+pub fn decrypt(config: &EncryptConfig) -> FedResult<()> {
+    unimplemented!()  //TODO @mark:
+}
