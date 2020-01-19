9d331b82e87c8666d500cd2e0d4cab4466282877
Mark <mverleg.noreply@gmail.com>
2020-01-19 23:51:51 +0100

    Logging of input arguments

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -1,6 +1,7 @@
 use ::file_endec::EncryptConfig;
 use ::structopt::StructOpt;
-use std::path::PathBuf;
+use ::std::path::PathBuf;
+use ::std::fmt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "FileEnc", about = "Securely encrypt one or more files using the given key.")]
@@ -21,8 +22,8 @@ pub struct EncryptArguments {
     #[structopt(short = "d", long, help = "Delete input files after successful encryption (overwrites garbage before delete).")]
     delete_input: bool,
 
-    #[structopt(parse(from_os_str), short = "o", long, default_value = ".", help = "Alternative output directory.")]
-    output_dir: PathBuf,
+    #[structopt(parse(from_os_str), short = "o", long, help = "Alternative output directory. If not given, output is saved alongside input.")]
+    output_dir: Option<PathBuf>,
 
     #[structopt(short = "e", long, default_value = ".enc", help = "Extension added to encrypted files.")]
     output_extension: String,
@@ -33,14 +34,68 @@ pub struct EncryptArguments {
 
 impl From<EncryptArguments> for EncryptConfig {
     fn from(args: EncryptArguments) -> Self {
-        unimplemented!()
+        assert!(args.key.is_some(), "key must be set before calling 'from'/'into'");
+        EncryptConfig::new(
+            args.files,
+            args.key.unwrap(),
+            args.debug,
+            args.overwrite,
+            args.delete_input,
+            args.output_dir,
+            args.output_extension,
+            args.dry_run,
+        )
+    }
+}
+
+impl fmt::Display for EncryptArguments {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        f.write_str("  files:\n")?;
+        for file in self.files.clone().into_iter() {
+            f.write_str("  - ")?;
+            f.write_str(file.to_string_lossy().as_ref())?;
+            f.write_str("\n")?;
+        }
+
+        //TODO @mark: absolute path?
+        match &self.output_dir {
+            Some(dir) => {
+                f.write_str("  output directory: ")?;
+                f.write_str(dir.to_string_lossy().as_ref())?
+            },
+            None => f.write_str("  output is stored alongside input")?,
+        }
+        f.write_str("\n")?;
+
+        f.write_str("  extension: ")?;
+        //TODO @mark: absolute path?
+        f.write_str(&self.output_extension)?;
+        f.write_str("\n")?;
+
+        // Currently, this is always "on", because printing is only used in debug mode.
+        f.write_str("  debug logging: ")?;
+        f.write_str(if self.debug { "on" } else { "off" })?;
+        f.write_str("\n")?;
+
+        f.write_str("  dry run: ")?;
+        f.write_str(if self.dry_run { "yes" } else { "no" })?;
+        f.write_str("\n")?;
+
+        f.write_str("  overwrite existing output files: ")?;
+        f.write_str(if self.overwrite { if self.dry_run { "no (overridden by dry run)" } else { "yes" }} else { "no" })?;
+        f.write_str("\n")?;
+
+        f.write_str("  delete input files: ")?;
+        f.write_str(if self.delete_input { if self.dry_run { "no (overridden by dry run)" } else { "yes" }} else { "no" })?;
+        f.write_str("\n")?;
+
+        Ok(())
     }
 }
 
 pub fn main() {
     let args = EncryptArguments::from_args();
-    if args.files.len() == 0 {
-
+    if args.debug {
+        println!("arguments provided:\n{}", args);
     }
-    println!("{:?}", args);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,84 @@
-pub mod util;
-pub mod header;
+use std::path::{Path, PathBuf};
 
 use crate::util::FedResult;
 
+pub mod util;
+pub mod header;
+
 #[derive(Debug)]
 pub struct EncryptConfig {
+    files: Vec<PathBuf>,
+    key: String,  //TODO @mark: change type
+    debug: bool,
+    overwrite: bool,
+    delete_input: bool,
+    output_dir: Option<PathBuf>,
+    output_extension: String,
+    dry_run: bool,
+}
 
+impl EncryptConfig {
+    pub fn new(
+        files: Vec<PathBuf>,
+        key: String,
+        debug: bool,
+        mut overwrite: bool,
+        mut delete_input: bool,
+        output_dir: Option<PathBuf>,
+        output_extension: String,
+        dry_run: bool,
+    ) -> Self {
+        assert!(files.len() >= 1);
+        if dry_run {
+            delete_input = false;
+            overwrite = false;
+        }
+        EncryptConfig {
+            files,
+            key,
+            debug,
+            overwrite,
+            delete_input,
+            output_dir,
+            output_extension,
+            dry_run,
+        }
+    }
+
+    pub fn files(&self) -> &[PathBuf] {
+        &self.files
+    }
+
+    pub fn key(&self) -> &str {
+        &self.key
+    }
+
+    pub fn debug(&self) -> bool {
+        self.debug
+    }
+
+    pub fn overwrite(&self) -> bool {
+        self.overwrite
+    }
+
+    pub fn delete_input(&self) -> bool {
+        self.delete_input
+    }
+
+    pub fn output_dir(&self) -> Option<&Path> {
+        match &self.output_dir {
+            Some(dir) => Some(dir.as_path()),
+            None => None,
+        }
+    }
+
+    pub fn output_extension(&self) -> &str {
+        &self.output_extension
+    }
+
+    pub fn dry_run(&self) -> bool {
+        self.dry_run
+    }
 }
 
 #[derive(Debug)]
