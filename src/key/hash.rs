202c7ae1445b6db15bab684173021d41094cc1ab
Mark <mverleg.noreply@gmail.com>
2020-01-24 00:04:04 +0100

    Attempt sha256 hashing

--- src/key/hash.rs
+++ src/key/hash.rs
@@ -1,28 +1,35 @@
 
 //TODO @mark: change to &mut [u8]? does that work with all algorithms?
 
+use ::std::num::NonZeroU32;
+
+use ::ring::pbkdf2::derive;
+use ::ring::pbkdf2::PBKDF2_HMAC_SHA256;
+
 use crate::header::KeyHashAlg;
 
 #[inline]
-pub fn hash(data: &mut Vec<u8>, algorithm: &KeyHashAlg) {
+pub fn hash(data: &[u8], salt: &[u8], algorithm: &KeyHashAlg) -> [u8; 32] {
     match algorithm {
         KeyHashAlg::SCrypt => hash_scrypt(data),
         KeyHashAlg::Argon2id => hash_argon2id(data),
-        KeyHashAlg::Sha256 => hash_sha256(data),
+        KeyHashAlg::Sha256 => hash_sha256(data, salt),
     }
 }
 
 #[inline]
-pub fn hash_scrypt(data: &mut [u8]) {
+pub fn hash_scrypt(data: &[u8]) -> [u8; 32] {
     unimplemented!()
 }
 
 #[inline]
-pub fn hash_argon2id(data: &mut [u8]) {
+pub fn hash_argon2id(data: &[u8]) -> [u8; 32] {
     unimplemented!()
 }
 
 #[inline]
-pub fn hash_sha256(data: &mut [u8]) {
-    unimplemented!()
+pub fn hash_sha256(data: &[u8], salt: &[u8]) -> [u8; 32] {
+    let mut salted = [0; 32];
+    derive(PBKDF2_HMAC_SHA256, NonZeroU32::new_unchecked(100), salt, data, &mut salted);
+    salted
 }
--- src/key/stretch.rs
+++ src/key/stretch.rs
@@ -11,12 +11,12 @@ pub fn stretch_key(raw_key: &Key, salt: &Salt, stretch_count: u64, key_hash_algo
     for key_hash_alg in key_hash_algorithms {
         println!("alg {:?} init", key_hash_alg);  //TODO @mark: TEMPORARY! REMOVE THIS!
         data.extend(&salt_bytes);
-        hash(&mut data, key_hash_alg);
+        data = hash(&mut data, &salt_bytes, key_hash_alg).into();
         for i in 0 .. stretch_count {
             println!("alg {:?} i = {}", key_hash_alg, i);  //TODO @mark: TEMPORARY! REMOVE THIS!
             data.extend(&salt_bytes);
             data.extend(&i.to_le_bytes());
-            hash(&mut data, key_hash_alg);
+            data = hash(&mut data, &salt_bytes, key_hash_alg).into();
         }
     }
     StretchKey::new(&data)
