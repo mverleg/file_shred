500d5a38d979d8ce802040fe4d8bca9a4e899cde
Mark <mverleg.noreply@gmail.com>
2020-01-23 22:50:11 +0100

    Move all benchmarks to a single file after all

--- src/bench.rs
+++ src/bench.rs
@@ -1,4 +1,5 @@
-use criterion::criterion_main;
+use ::criterion::criterion_main;
+use ::criterion::criterion_group;
 
 //#[cfg(test)]
 //fn main() {
@@ -7,5 +8,44 @@ use criterion::criterion_main;
 //    sha256_benchmark();
 //}
 
-criterion_main!(benches);
+#[cfg(test)]
+mod hash {
+    use ::criterion::black_box;
+    use ::criterion::Criterion;
 
+    use ::file_endec::key::hash::hash_argon2id;
+    use ::file_endec::key::hash::hash_scrypt;
+    use ::file_endec::key::hash::hash_sha256;
+
+    fn get_data() -> Vec<u8> {
+        vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    }
+
+    pub fn scrypt_benchmark(c: &mut Criterion) {
+        let mut data = get_data();
+        c.bench_function("scrypt", |b| b.iter(
+            || hash_scrypt(black_box(&mut data))));
+    }
+
+    pub fn argon2id_benchmark(c: &mut Criterion) {
+        let mut data = get_data();
+        c.bench_function("argon2id", |b| b.iter(
+            || hash_argon2id(black_box(&mut data))));
+    }
+
+    pub fn sha256_benchmark(c: &mut Criterion) {
+        let mut data = get_data();
+        c.bench_function("sha256", |b| b.iter(
+            || hash_sha256(black_box(&mut data))));
+    }
+}
+
+criterion_group!(hash_bench,
+    hash::scrypt_benchmark,
+    hash::argon2id_benchmark,
+    hash::sha256_benchmark,
+);
+
+criterion_main!(
+    hash_bench,
+);
--- src/key/hash.rs
+++ src/key/hash.rs
@@ -8,7 +8,7 @@ pub fn hash(data: &mut Vec<u8>, algorithm: &KeyHashAlg) {
     match algorithm {
         KeyHashAlg::SCrypt => hash_scrypt(data),
         KeyHashAlg::Argon2id => hash_argon2id(data),
-        KeyHashAlg::Sha256 => ash_sha256(data),
+        KeyHashAlg::Sha256 => hash_sha256(data),
     }
 }
 
@@ -23,43 +23,6 @@ pub fn hash_argon2id(data: &mut [u8]) {
 }
 
 #[inline]
-pub fn ash_sha256(data: &mut [u8]) {
+pub fn hash_sha256(data: &mut [u8]) {
     unimplemented!()
-}
-
-#[cfg(test)]
-mod tests {
-    use ::criterion::black_box;
-    use ::criterion::Criterion;
-    use ::criterion::criterion_group;
-
-    use super::*;
-
-    fn get_data() -> Vec<u8> {
-        vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-    }
-
-    pub fn scrypt_benchmark(c: &mut Criterion) {
-        let mut data = get_data();
-        c.bench_function("scrypt", |b| b.iter(
-            || hash_scrypt(black_box(&mut data))));
-    }
-
-    pub fn argon2id_benchmark(c: &mut Criterion) {
-        let mut data = get_data();
-        c.bench_function("argon2id", |b| b.iter(
-            || hash_argon2id(black_box(&mut data))));
-    }
-
-    pub fn sha256_benchmark(c: &mut Criterion) {
-        let mut data = get_data();
-        c.bench_function("sha256", |b| b.iter(
-            || ash_sha256(black_box(&mut data))));
-    }
-
-    criterion_group!(hash_bench,
-            scrypt_benchmark,
-            argon2id_benchmark,
-            sha256_benchmark,
-    );
 }
