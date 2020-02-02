f40afdf27841280bb6aa8a0845c13c0550c2aac9
Mark <mverleg.noreply@gmail.com>
2020-02-02 14:44:35 +0100

    Hash strategy is linked to version number, in case parameters change

--- src/key/hash.rs
+++ src/key/hash.rs
@@ -23,18 +23,27 @@ pub fn hash(data: &[u8], salt: &[u8], algorithm: &KeyHashAlg) -> Vec<u8> {
 }
 
 lazy_static! {
+    // NOTE: if the hash parameters ever change, then KeyHashAlg must be extended to reflect that,
+    // to make sure that anything encrypted before can still be decrypted.
     // https://crypto.stackexchange.com/questions/35423
     static ref SCRYPT_PARAMS: ScryptParams = ScryptParams::new(14, 8, 1);
 }
 
 lazy_static! {
-    static ref ARGON_CONFIG: Argon2 = Argon2::default(Variant::Argon2i);
+    // NOTE: if the hash parameters ever change, then KeyHashAlg must be extended to reflect that,
+    // to make sure that anything encrypted before can still be decrypted.
+    static ref ARGON_CONFIG: Argon2 = Argon2::new(30, 8, 4096, Variant::Argon2i).unwrap();
+}
+
+lazy_static! {
+    // NOTE: if the hash parameters ever change, then KeyHashAlg must be extended to reflect that,
+    // to make sure that anything encrypted before can still be decrypted.
+    static ref SHA_REPS: NonZeroU32 = NonZeroU32::new(70_000).unwrap();
 }
 
 #[inline]
 pub fn hash_scrypt(data: &[u8], salt: &[u8]) -> Vec<u8> {
     let mut output = vec![0; 32];
-    //TODO @mark: put all changeable params in header
     scrypt(salt, data, &*SCRYPT_PARAMS, &mut output);
     output
 }
@@ -49,8 +58,7 @@ pub fn hash_argon2i(data: &[u8], salt: &[u8]) -> Vec<u8> {
 #[inline]
 pub fn hash_sha256(data: &[u8], salt: &[u8]) -> Vec<u8> {
     let mut output = vec![0; 32];
-    //TODO @mark: put repetitions in header
-    derive(PBKDF2_HMAC_SHA512, NonZeroU32::new(3000).unwrap(), salt, data, &mut output);
+    derive(PBKDF2_HMAC_SHA512, *SHA_REPS, salt, data, &mut output);
     output
 }
 
@@ -60,31 +68,22 @@ mod tests {
 
     #[test]
     fn test_hash_scrypt() {
-        for i in 0..100 {
-            let hashed = hash_scrypt(&vec![1; 32], &vec![2; 32]);
-            let expected: Vec<u8> = vec![28, 234, 128, 254, 84, 184, 1, 65, 151, 233, 171, 200, 65, 29, 106, 154, 65, 227, 136, 17, 6, 197, 135, 104, 124, 206, 31, 105, 12, 197, 92, 30];
-            assert_eq!(expected, hashed);
-            println!("{}", i);  //TODO @mark: TEMPORARY! REMOVE THIS!
-        }
+        let hashed = hash_scrypt(&vec![1; 32], &vec![2; 32]);
+        let expected: Vec<u8> = vec![28, 234, 128, 254, 84, 184, 1, 65, 151, 233, 171, 200, 65, 29, 106, 154, 65, 227, 136, 17, 6, 197, 135, 104, 124, 206, 31, 105, 12, 197, 92, 30];
+        assert_eq!(expected, hashed);
     }
 
     #[test]
     fn test_hash_argon2id() {
-        for i in 0..100 {
-            let hashed = hash_argon2i(&vec![1; 32], &vec![2; 32]);
-            let expected: Vec<u8> = vec![154, 167, 243, 76, 224, 11, 190, 190, 16, 53, 5, 112, 123, 111, 242, 242, 217, 204, 29, 52, 124, 27, 170, 145, 157, 167, 52, 43, 82, 205, 141, 89];
-            assert_eq!(expected, hashed);
-            println!("{}", i);  //TODO @mark: TEMPORARY! REMOVE THIS!
-        }
+        let hashed = hash_argon2i(&vec![1; 32], &vec![2; 32]);
+        let expected: Vec<u8> = vec![114, 139, 48, 2, 98, 196, 133, 19, 232, 144, 6, 149, 44, 68, 116, 152, 233, 120, 110, 205, 15, 29, 180, 181, 4, 86, 84, 153, 228, 231, 106, 225];
+        assert_eq!(expected, hashed);
     }
 
     #[test]
     fn test_hash_sha256() {
-        for i in 0..100 {
-            let hashed = hash_sha256(&vec![1; 32], &vec![2; 32]);
-            let expected: Vec<u8> = vec![188, 243, 63, 250, 102, 212, 13, 123, 200, 237, 71, 176, 152, 157, 122, 117, 92, 128, 226, 83, 139, 63, 234, 131, 207, 209, 204, 26, 227, 96, 247, 8];
-            assert_eq!(expected, hashed);
-            println!("{}", i);  //TODO @mark: TEMPORARY! REMOVE THIS!
-        }
+        let hashed = hash_sha256(&vec![1; 32], &vec![2; 32]);
+        let expected: Vec<u8> = vec![89, 92, 124, 234, 51, 101, 87, 71, 223, 104, 235, 37, 116, 52, 18, 253, 105, 30, 196, 19, 174, 103, 43, 152, 200, 52, 241, 160, 102, 155, 118, 89];
+        assert_eq!(expected, hashed);
     }
 }
