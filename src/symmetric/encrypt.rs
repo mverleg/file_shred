555a6be9a021b1b8987d56d2f831038458ab43d6
Mark <mverleg.noreply@gmail.com>
2020-02-11 23:03:17 +0100

    Tests for aes_ctr

--- src/symmetric/encrypt.rs
+++ src/symmetric/encrypt.rs
@@ -25,8 +25,11 @@ pub fn encrypt_aes256(mut data: Vec<u8>, key: &StretchKey, salt: &Salt) -> FedRe
     //let cipher = Aes256::new(&key_ga);
     //cipher.encrypt_block(&data);
 
-    println!("C") ; //TODO @mark: TEMPORARY! REMOVE THIS!
-    let key = GenericArray::from_slice(&key.key_data.unsecure());
+    println!("C1") ; //TODO @mark: TEMPORARY! REMOVE THIS!
+    dbg!(&key.key_data.unsecure());  //TODO @mark: TEMPORARY! REMOVE THIS!
+    //let key_data = ;
+    println!("C2") ; //TODO @mark: TEMPORARY! REMOVE THIS!
+    let key = GenericArray::from_slice(key.key_data.unsecure());
     println!("D") ; //TODO @mark: TEMPORARY! REMOVE THIS!
     let nonce = GenericArray::from_slice(&salt.salt);
     println!("E") ; //TODO @mark: TEMPORARY! REMOVE THIS!
@@ -34,7 +37,7 @@ pub fn encrypt_aes256(mut data: Vec<u8>, key: &StretchKey, salt: &Salt) -> FedRe
     println!("F") ; //TODO @mark: TEMPORARY! REMOVE THIS!
     cipher.apply_keystream(&mut data);
     println!("G") ; //TODO @mark: TEMPORARY! REMOVE THIS!
-    assert_eq!(data, [6, 245, 126, 124, 180, 146, 37]);
+    //assert_eq!(data, [6, 245, 126, 124, 180, 146, 37]);
 
     unimplemented!()
 }
@@ -46,6 +49,37 @@ pub fn encrypt_blowfish(mut data: Vec<u8>, key: &StretchKey) -> FedResult<Vec<u8
 #[cfg(test)]
 mod tests {
     use super::*;
+    use aes_ctr::Aes128Ctr;
+
+    #[test]
+    fn aes_ctr_sanity_check_demo() {
+        let mut data = [1, 2, 3, 4, 5, 6, 7];
+        let key = GenericArray::from_slice(b"very secret key.");
+        let nonce = GenericArray::from_slice(b"and secret nonce");
+        let mut cipher = Aes128Ctr::new(&key, &nonce);
+        cipher.apply_keystream(&mut data);
+        assert_eq!(data, [6, 245, 126, 124, 180, 146, 37]);
+        cipher.seek(0);
+        cipher.apply_keystream(&mut data);
+        assert_eq!(data, [1, 2, 3, 4, 5, 6, 7]);
+    }
+
+    #[test]
+    fn aes_ctr_sanity_check_own() {
+        let salt = Salt::static_for_test(123_456_789);
+        let mut input = vec![
+            00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15,
+            16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+            32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+            48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
+            64, 65, 66, 67, 68, 69, 70,
+        ];
+        let key = GenericArray::from_slice("s3cr3t!".as_bytes());
+        let nonce = GenericArray::from_slice(&[1, 2, 3, 4, 5, 6, 7, 8,
+            1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8]);
+        let mut cipher = Aes256Ctr::new(&key, &nonce);
+        cipher.apply_keystream(&mut input);
+    }
 
     #[test]
     fn aes256_small() {
