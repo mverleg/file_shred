fe3677fd504edcf9a506b633858f3573ced19dea
Mark <mverleg.noreply@gmail.com>
2020-02-16 14:48:40 +0100

    Succesfully find test file location

--- src/files/scan.rs
+++ src/files/scan.rs
@@ -1,6 +1,9 @@
+#[cfg(test)] use ::std::env;
 use ::std::ffi::OsStr;
 use ::std::fs;
-use ::std::path::{Path, PathBuf};
+use ::std::path::Path;
+use ::std::path::PathBuf;
+
 #[cfg(test)] use ::lazy_static::lazy_static;
 
 use crate::util::FedResult;
@@ -38,19 +41,29 @@ pub fn get_enc_files_direct(dir: &Path) -> FedResult<Vec<PathBuf>> {
 lazy_static! {
     static ref TEST_FILE_DIR: PathBuf = {
         // Try to find relative to target dir.
-        let target_dir = env!("CARGO_TARGET_DIR");
-        let mut test_files_dir = PathBuf::from(target_dir).push("..").push("..").push("test_files");
-        let original_file = test_files_dir.clone().push("original.png");
+        let mut test_files_dir: PathBuf = {
+            let mut p = PathBuf::from(std::env::current_exe().unwrap());
+            p.pop();
+            p.pop();
+            p.pop();
+            p.pop();
+            p.push("test_files");
+            p
+        };
+        let mut original_file = test_files_dir.clone();
+        original_file.push("original.png");
         if !original_file.is_file() {
             // Perhaps the target dir is not in the default location, try something else.
-            let test_file_dir_env = env!("ENDEC_TEST_FILE_DIR");
-            if test_file_dir_env.is_empty() {
-                panic!(format!("Expected test files at '{}' but they were not found; set the environment variable 'ENDEC_TEST_FILE_DIR' to the test file location.", test_files_dir.to_string_lossy()));
-            }
-            test_files_dir = PathBuf::from(test_file_dir_env);
-            let original_file = test_files_dir.clone().push("original.png");
-            if !original_file.is_file() {
-                panic!(format!("Expected test files at '{}' based on environment variable 'ENDEC_TEST_FILE_DIR', but the files were not found.", test_files_dir.to_string_lossy()));
+            match env::var("ENDEC_TEST_FILE_DIR") {
+                Ok(test_file_dir_env) => {
+                    test_files_dir = PathBuf::from(test_file_dir_env);
+                    original_file = test_files_dir.clone();
+                    original_file.push("original.png");
+                    if !original_file.is_file() {
+                        panic!(format!("Expected test files at '{}' based on environment variable 'ENDEC_TEST_FILE_DIR', but the files were not found.", test_files_dir.to_string_lossy()));
+                    }
+                },
+                Err(err) => panic!(format!("Expected test files at '{}' but they were not found; set the environment variable 'ENDEC_TEST_FILE_DIR' to the test file location.", test_files_dir.to_string_lossy())),
             }
         }
         test_files_dir
@@ -59,8 +72,8 @@ lazy_static! {
 
 #[cfg(test)]
 pub fn test_files() -> Vec<PathBuf> {
-    let mut test_files_dir = TEST_FILE_DIR.clone();
-    get_enc_files_direct(test_files_dir).unwrap()
+    let test_files_dir = TEST_FILE_DIR.clone();
+    get_enc_files_direct(&test_files_dir).unwrap()
 }
 
 #[cfg(test)]
@@ -69,6 +82,7 @@ mod tests {
 
     #[test]
     fn find_files() {
-        get_enc_files_direct(&PathBuf::from("test_files/"))
+        let files = get_enc_files_direct(&*TEST_FILE_DIR).unwrap();
+        assert!(files.len() >= 1);
     }
 }
