8c95d25d6fc850a29aaca00f77006ef550f6647f
Mark <mverleg.noreply@gmail.com>
2020-03-29 17:01:16 +0200

    Use new naming scheme

--- src/erase/rename.rs
+++ src/erase/rename.rs
@@ -1,19 +1,25 @@
 use ::std::fs;
 use ::std::path::Path;
 use ::std::path::PathBuf;
+use ::number2name::BASE32HUMAN;
 
 use crate::util::errors::add_err;
 use crate::util::FedResult;
-use crate::util::names::generate_name;
+
+fn generate_name(number: u32) -> String {
+    // Character set: abcdefghjkmnpqrstuvwxyz23456789_
+    let mut name = BASE32HUMAN.encode(number as u64);
+    name.push_str(".tmp");
+    name
+}
 
 pub fn repeatedly_rename_file(original_pth: &Path, reps: u32, verbose: bool) -> FedResult<PathBuf> {
     let mut renamed = reps;
     let mut old_path = original_pth.to_owned();
     for iter in 0..100 * reps {
-        let name = format!("{}.tmp", &generate_name(iter as u64));
         let new_path = {
             let mut p = old_path.clone();
-            p.set_file_name(name);
+            p.set_file_name(generate_name(iter));
             p
         };
         if new_path.exists() {
@@ -53,7 +59,7 @@ mod tests {
         path.push("original.file");
         fs::write(&path, &data).unwrap();
         let new_pth = repeatedly_rename_file(&path, 5, true).unwrap();
-        assert_eq!("BAAA.tmp", new_pth.file_name().unwrap());
+        assert_eq!("e.tmp", new_pth.file_name().unwrap());
         assert_eq!(&*data, fs::read(new_pth).unwrap().as_slice());
     }
 
@@ -68,12 +74,14 @@ mod tests {
         let data = b"hello world, this is test data";
         let temp_handle = tempdir().unwrap();
         let mut path = temp_handle.path().to_owned();
-        make_collision_file(&path, "AQAA.tmp");
-        make_collision_file(&path, "BAAA.tmp");
+        make_collision_file(&path, "e.tmp");
+        make_collision_file(&path, "z.tmp");
+        make_collision_file(&path, "_.tmp");
+        make_collision_file(&path, "aa.tmp");
         path.push("original.file");
         fs::write(&path, &data).unwrap();
-        let new_pth = repeatedly_rename_file(&path, 5, true).unwrap();
-        assert_eq!("BgAA.tmp", new_pth.file_name().unwrap());
+        let new_pth = repeatedly_rename_file(&path, 30, true).unwrap();
+        assert_eq!("ab.tmp", new_pth.file_name().unwrap());
         assert_eq!(&*data, fs::read(new_pth).unwrap().as_slice());
     }
 }
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,4 @@
 pub use errors::FedResult;
 
 #[allow(clippy::module_inception)]
-pub mod names;
 pub mod errors;
