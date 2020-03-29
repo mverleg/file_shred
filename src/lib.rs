879f029186cf6b69465b9f1ddd634d79847639e1
Mark <mverleg.noreply@gmail.com>
2020-03-29 20:17:39 +0200

    Start demo test

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 pub use crate::config::conf::ShredConfig;
 pub use crate::config::typ::Verbosity;
-pub use crate::util::errors::ShredResult;
-
 use crate::erase::orchestrate::delete_file;
 use crate::inspect::collect::collect_file_info;
 use crate::util::cli::confirm_delete;
+pub use crate::util::errors::ShredResult;
 
 mod config;
 mod inspect;
@@ -27,9 +26,37 @@ pub fn shred(config: &ShredConfig) -> ShredResult<()> {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use ::std::fs::File;
+    use ::std::io::Write;
+    use ::std::path::Path;
+    use ::std::path::PathBuf;
+
+    use ::tempfile::tempdir;
+    use crate::{shred, ShredConfig};
+
+    fn make_file(dir: &Path, name: &str) -> PathBuf {
+        let mut pth = dir.to_owned();
+        pth.push(name);
+        let mut file1 = File::create(&pth).unwrap();
+        file1.write_all(b"Test file content for ").unwrap();
+        file1.write_all(name.as_bytes()).unwrap();
+        return pth;
+    }
 
     #[test]
     fn demo() {
-        unimplemented!()  //TODO @mark:
+        let dir = tempdir().unwrap();
+        let pth1 = make_file(dir.path(), "file_1.txt");
+        let pth2 = make_file(dir.path(), "other_file.bye");
+        let config = ShredConfig::new(
+            vec![pth1, pth2],  // files
+            false,  // confirmation_prompt
+            Verbosity::Debug,  // verbosity
+            false,  // keep_files
+            6, // overwrite_count
+            3, // rename_count
+        );
+        shred(&config).unwrap()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,6 @@ use ::structopt::StructOpt;
 
 use file_shred::{shred, ShredConfig, ShredResult, Verbosity};
 
-//TODO @mark: option to ask if the user is sure?
-
 #[derive(Debug, StructOpt)]
 #[structopt(
     name = "Shred",
